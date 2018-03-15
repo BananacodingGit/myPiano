3fc1fe114af89bf29ac1559251a918760dbd9f55
BananacodinGit <youngjin.kim@bananacoding.ac>
2018-03-15 15:10:32 +0900

    delete millis(), change value name

--- foil_piano/foil_piano.ino
+++ foil_piano/foil_piano.ino
@@ -28,49 +28,43 @@ void setup() {
 }
 
 void loop() {
-  long total1 =  cs_2_3.capacitiveSensor(30);
-  long total2 =  cs_2_4.capacitiveSensor(30);
-  long total3 =  cs_2_5.capacitiveSensor(30);
-  long total4 =  cs_2_6.capacitiveSensor(30);
-  long total5 =  cs_2_7.capacitiveSensor(30);
-  long total6 =  cs_2_8.capacitiveSensor(30);
-  long total7 =  cs_2_9.capacitiveSensor(30);
-  long total8 =  cs_2_10.capacitiveSensor(30);
+  long C_4 =  cs_2_3.capacitiveSensor(30);
+  long D_4 =  cs_2_4.capacitiveSensor(30);
+  long E_4 =  cs_2_5.capacitiveSensor(30);
+  long F_4 =  cs_2_6.capacitiveSensor(30);
+  long G_4 =  cs_2_7.capacitiveSensor(30);
+  long A_4 =  cs_2_8.capacitiveSensor(30);
+  long B_4 =  cs_2_9.capacitiveSensor(30);
+  long C_5 =  cs_2_10.capacitiveSensor(30);
 
-  long start = millis();
+  Serial.print(C_4);
+  Serial.print("\t");
+  Serial.print(D_4);
+  Serial.print("\t");
+  Serial.print(E_4);
+  Serial.print("\t");
+  Serial.print(F_4);
+  Serial.print("\t");
+  Serial.print(G_4);
+  Serial.print("\t");
+  Serial.print(A_4);
+  Serial.print("\t");
+  Serial.print(B_4);
+  Serial.print("\t");
+  Serial.println(C_5);
 
-  Serial.print(millis() - start);
-  Serial.print("\t");
+  if (C_4 > capacity) {tone(speaker,261);}
+  if (D_4 > capacity) {tone(speaker,294);}
+  if (E_4 > capacity) {tone(speaker,329);}
+  if (F_4 > capacity) {tone(speaker,349);}
+  if (G_4 > capacity) {tone(speaker,392);}
+  if (A_4 > capacity) {tone(speaker,440);}
+  if (B_4 > capacity) {tone(speaker,493);}
+  if (C_5 > capacity) {tone(speaker,523);}
 
-  Serial.print(total1);
-  Serial.print("\t");
-  Serial.print(total2);
-  Serial.print("\t");
-  Serial.print(total3);
-  Serial.print("\t");
-  Serial.print(total4);
-  Serial.print("\t");
-  Serial.print(total5);
-  Serial.print("\t");
-  Serial.print(total6);
-  Serial.print("\t");
-  Serial.print(total7);
-  Serial.print("\t");
-  Serial.println(total8);
-
-  if (total1 > capacity) {tone(speaker,261);}
-  if (total2 > capacity) {tone(speaker,294);}
-  if (total3 > capacity) {tone(speaker,329);}
-  if (total4 > capacity) {tone(speaker,349);}
-  if (total5 > capacity) {tone(speaker,392);}
-  if (total6 > capacity) {tone(speaker,440);}
-  if (total7 > capacity) {tone(speaker,493);}
-  if (total8 > capacity) {tone(speaker,523);}
-
-  if (total1<=capacity & total2<=capacity & 
-      total3<=capacity & total4<=capacity & 
-      total5<=capacity & total6<=capacity &  
-      total7<=capacity & total8<=capacity)
+  if (C_4<=capacity & D_4<=capacity & E_4<=capacity & 
+      F_4<=capacity & G_4<=capacity & A_4<=capacity &  
+      B_4<=capacity & C_5<=capacity)
   {
     noTone(speaker);
   }
